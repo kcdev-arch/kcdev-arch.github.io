605ec290e4f90654cacbde583c8b573b0e58d3f9
kcdev-arch <kc@xmail.rf.gd>
2025-09-18 19:33:02 +0200

    vDEV1.0

---
+++ script.js
@@ -0,0 +1,6 @@
+function openSubject(subject) {
+    // Zatím jen alert, později může přesměrovat na detailní stránku
+    alert("Otevřít předmět: " + subject);
+    // Příklad přesměrování:
+    // window.location.href = subject.toLowerCase() + ".html";
+}
