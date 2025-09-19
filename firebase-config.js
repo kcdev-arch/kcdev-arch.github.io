6445df6676e1cce56f2c6c1f90df0804cd3a3b2b
kcdev-arch <kc@xmail.rf.gd>
2025-09-19 21:26:05 +0200

    vDEV2.1

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,16 @@
+// firebase-config.js
 const firebaseConfig = {
-    apiKey: "AIzaSyAsg9CgO_WZC1TIGk-HiA6uckodWDLET3w",
-    authDomain: "studyhub-161cb.firebaseapp.com",
-    projectId: "studyhub-161cb",
-    storageBucket: "studyhub-161cb.firebasestorage.app",
-    messagingSenderId: "270475349190",
-    appId: "1:270475349190:web:5dd3f8894ac31eb23a9bc7",
-    measurementId: "G-9BBZZS59YR"
-  };
+  apiKey: "AIzaSyAsg9CgO_WZC1TIGk-HiA6uckodWDLET3w",
+  authDomain: "studyhub-161cb.firebaseapp.com",
+  projectId: "studyhub-161cb",
+  storageBucket: "studyhub-161cb.firebasestorage.app",
+  messagingSenderId: "270475349190",
+  appId: "1:270475349190:web:5dd3f8894ac31eb23a9bc7",
+  measurementId: "G-9BBZZS59YR"
+};
 
-  // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// Initialize Firebase with the global syntax
+firebase.initializeApp(firebaseConfig);
+
+// Get a reference to the Firestore service
+const db = firebase.firestore();
