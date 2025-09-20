7d21437f405112d433d818bef1bd31790b332028
kcdev-arch <kc@xmail.rf.gd>
2025-09-20 17:49:47 +0200

    Update admin.js

--- admin.js
+++ admin.js
@@ -9,7 +9,7 @@ const statusMessageDiv = document.getElementById('status-message');
 
 // Seznam povolených e-mailových adres
 const allowedEmails = [
-    "karelcerny404@seznam.cz" // SEM VLOŽTE SVŮJ GMAIL ÚČET
+    "linuxsmajlik@gmail.com" // SEM VLOŽTE SVŮJ GMAIL ÚČET
 ];
 
 const provider = new firebase.auth.GoogleAuthProvider();
